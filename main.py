f5142c3b5015f0b34028795e4e09033ec98eb2f1
KazeFX <1071525+KazeFX@users.noreply.github.com>
2026-05-02 13:50:13 +0200

    Added some more classes and ship rotation.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
+import player
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH 
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,7 +21,13 @@ def main():
                 return
             pass
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        tick_value = fps.tick(60)
+        dt = tick_value / 1000
+        
+        
 
 
 if __name__ == "__main__":
